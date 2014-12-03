69690a1714fa4428e13c2eb7dca0b65e0dca5b38
Fred Shequine <fshequin@gmail.com>
2014-12-02 23:20:44 -0500

    updates to wp core, plugins, bootstrap-sass
    Signed-off-by: Fred Shequine <fshequin@gmail.com>

--- wp-content/themes/boot-theme/single.php
+++ wp-content/themes/boot-theme/single.php
@@ -6,13 +6,13 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12 main-content">
-      
+        
         <?php if(have_posts()) : ?>
         <?php while(have_posts()) : the_post(); ?>
         
         <div class="post-content">
-          <h1>News</h1>
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h1><a href="/news/">News</a></h1>
+          <h2><?php the_title(); ?></h2>
           <?php the_content(); ?>
           <p>&nbsp;</p>
         </div>
